3ff5f2f23767b85c320a2bf4e759a3243acf573f
j-curtis <jcurtis2@u.rochester.edu>
2015-08-09 23:17:59 -0700

    Update xas-convolve.cpp
    Added convolution routine; finished preliminary version.

--- xas-convolve.cpp
+++ xas-convolve.cpp
@@ -38,12 +38,12 @@ void parseInt(string label, string value, string flag, int & variable){
 double linearInterpolate(double x, double x1, double f1, double x2, double f2){
   //Given x, x1, x2, f1, f2, this will interpolate f(x) as:
   //f(x) = f1 if x<=x1
-  //f(x) = f1 + f2*(x-x1)/(x2-x1) if x1<x<=x2
-  //f(x) = f2 if x>x2
+  //f(x) = f1 + f2*(x-x1)/(x2-x1) if x1<x<x2
+  //f(x) = f2 if x>=x2
   if(x<=x1){
     return f1;
   }
-  else if(x>x2){
+  else if(x>=x2){
     return f2;
   }
   else{
@@ -130,6 +130,7 @@ int main(int argc, char * argv []){
     parseInt(label,value,"num_xps_steps",num_xps_steps);
     parseInt(label,value,"num_xas_steps",num_xas_steps);
   }
+  infile.close();
   
   //Now we do value checking 
   if(num_w_steps <= 2){
@@ -156,6 +157,101 @@ int main(int argc, char * argv []){
   xas = new double[num_xas_steps];
   out_freqs = genGrid(num_w_steps,min_w,max_w); //Generate a uniform list of frequencies from scratch
   out = new double[num_w_steps];  //This will be generated from the convolution
+  
+  //Open up XPS and XAS files and read the data in
+  ifstream xpsfile(XPSFILE);
+  string xpsline;
+  int xpscount = 0; //To increment through each line of the array that we are filling
+  while(getline(xpsfile,xpsline)){
+    //We now parse for 3 tokens, each a double 
+    string token1, token2, token3;
+    istringstream ss(xpsline);  //Extracts each token
+    ss>>token1>>token2>>token3;
+    //We only want the first token (frequency), and the third token (xps)
+    xps_freqs[xpscount] = atof(token1.c_str()); //We must also convert it to a double 
+    xps[xpscount] = atof(token3.c_str()); //We must also convert this to a double 
+    xpscounter++; //Increment the counter
+  }
+  xpsfile.close();  //Close the file
+  
+  ifstream xasfile(XASFILE);
+  string xasline;
+  int xascount = 0; //To increment through each line of the array that we are filling
+  while(getline(xasfile,xasline)){
+    //We now parse for 2 tokens, each a double 
+    string token1, token2;
+    istringstream ss(xasline);  //Extracts each token
+    ss>>token1>>token2;
+    xas_freqs[xascount] = atof(token1.c_str()); //We must also convert it to a double 
+    xas[xascount] = atof(token2.c_str()); //We must also convert this to a double 
+    xascounter++; //Increment the counter
+  }
+  xasfile.close();  //Close the file
+  
+  //Now all the data arrays have been filled, we may perform the convolution
+  //We use a double loop to implement the integral
+  //We do NOT use trapezoidal rule
+  //We precompute delta_w for convinience
+  //We also use this loop to output the results to the output file, to save on the number of loops
+  ofstream outfile(OUTFILE);
+  
+  double delta_w = out_freqs[1]-out_freqs[0];
+  
+  for(int i = 0; i < num_w_steps; i++){
+    out[i] = 0.0; //We initialize to zero
+    //Now we use the formula
+    //out(w) = int_{wmin}^{wmax} dw' xps(w')xas(w-w')
+    //We use linear interpolation to comute the integrands at each integration point 
+    for(int j = 0; j < num_w_steps; j++){
+      //We compute the snapped to grid value of j for the xas and xps grids 
+      //We also need i snapped to the xas grid (to compute xas(w-w') )
+      double w = out_freqs[i];  //The value of w
+      double u - out_freqs[j];  //The value of w'
+      
+      int j_xps = snapToGrid(u,num_xps_steps,xps_freqs); //The value of j snapped to the xps grid
+      //Thus, j_xps is between 0 and num_xps_steps - 1
+      int i_xas = snapToGrid(w-u,num_xas_steps,xas_freqs); //The value of i-j snapped to the xas grid
+      //Thus, this takes values between 0 and num_xas_steps
+      
+      //Now we compute the linearly extrapolated xas and xps for w and u
+      //We have to make sure we don't go out of array bounds when computing the right endpoint for the linear extrapolations
+      double xps_term = 0.0;
+      double xas_term = 0.0;
+      
+      if(j_xps < num_xps_steps-2){
+        xps_term = linearInterpolate(u,xps_freqs[j_xps],xps_freqs[j_xps+1],xps[j_xps],xps[j_xps+1]);
+      } 
+      else{
+        xps_term = linearInterpolate(u,xps_freqs[j_xps],xps_freqs[j_xps],xps[j_xps],xps[j_xps]);
+        //Note that is j_xps is always returned to be, at most num_xps_steps - 1, then when x = x2 = x1 it will return f2
+      }
+      
+      //Now we must do the same for the xas term
+      if(i_xas < num_xas_steps - 2){
+        xas_term = linearInterpolate(w-u,xas_freqs[i_xas],xas_freqs[i_xas+1],xas[i_xas],xas[i_xas+1]);
+      }
+      else{
+        xas_term = linearInterpolate(w-u,xas_freqs[i_xas],xas_freqs[i_xas],xas[i_xas],xas[i_xas]);
+      }
+      
+      out[i] += delta_w *xas_term*xps_term; //Add to the integral
+    }
+    
+    //And we write this term to a file
+    outfile<<out_freq[i]<<" "<<out[i]<<endl;
+  }
+  //And we close the output file
+  outfile.close();
+  
+  //Delete allocated memory
+  delete [] xps_freqs;
+  delete [] xps;
+  delete [] xas_freqs;
+  delete [] xas;
+  delete [] out_freqs;
+  delete [] out;
+  
+  return 0;
 }
 
 
