a35055d4ecd69ea5ecff7970089ac5bc40d5518c
j-curtis <jcurtis2@u.rochester.edu>
2015-08-09 22:39:08 -0700

    Update xas-convolve.cpp
    In main: added parsing of an input file, value checking for input parameters, and array allocation.

--- xas-convolve.cpp
+++ xas-convolve.cpp
@@ -69,8 +69,94 @@ int snapToGrid(double x, int size, double * arry){
   }
 }
 
+//This function generates an array of N uniformly spaced values from min to max
+//It returns a pointer to the first element of the array
+//The returned array must be freed at the end 
+double * genGrid(int Num, double min, double max){
+  double * val = new double[Num];
+  double delta = (max-min)/double(Num-1);
+  
+  for(int i = 0; i < Num; i++){
+    val[i] = min + double(i)*delta;
+  }
+  
+  return val;
+}
 
-
+//Now the main method
+int main(int argc, char * argv []){
+  //We check to make sure they have the right number of arguments
+  if(argc!=2){
+    cout<<"Incorrect usage. Usage is: 'program_name' INPUTFILE "<<endl;
+    return 0;
+  }
+  
+  //Now we define the variables we will use 
+  double min_w = 0.0; //The minimum frequency we compute the convolution for
+  double max_w = 0.0; //The maximum frequency we compute the convolution for
+  int num_w_steps = 3;//The number of points we compute the convolution for
+  int num_xps_steps;  //The number of points in the xps array
+  int num_xas_steps;  //The number of points in the xas array
+  
+  //These are the arrays for the input and output data
+  double * xps_freqs; //The frequencies for the input xps 
+  double * xps;   //The input xps values for each frequency
+  double * xas_freqs; //The frequencies for the input xas
+  double * xas;   //The input xas values for each frequency
+  double * out_freqs; //The output frequencies
+  double * out;   //The output values at each frequency
+  
+  string XPSFILE; //The file where the XPS is input from
+  string XASFILE; //The file where the XAS is input from
+  string OUTFILE; //The file where the output is written to
+  
+  //Now we parse for these values
+  ifstream infile(argv[1]);
+  string inline;
+  while(getline(infile,inline)){
+    //We parse for tokens of label, values pairs
+    string label, value;
+    istringstream ss(inline); //This is what actually breaks the line into tokens
+    ss>>label>>value;
+    //We use the variable name as a label 
+    parseStr(label,value,"XPSFILE",XPSFILE);
+    parseStr(label,value,"XASFILE",XASFILE);
+    parseStr(label,value,"OUTFILE",OUTFILE);
+    
+    parseInt(label,value,"num_w_steps",num_w_steps);
+    parseDbl(label,value,"min_w",min_w);
+    parseDbl(label,value,"max_w",max_w);
+    
+    parseInt(label,value,"num_xps_steps",num_xps_steps);
+    parseInt(label,value,"num_xas_steps",num_xas_steps);
+  }
+  
+  //Now we do value checking 
+  if(num_w_steps <= 2){
+    cout<<"Error: insufficient number of output frequency steps (<3)."<<endl;
+    return 0;
+  }
+  if(num_xps_steps <= 2){
+    cout<<"Error: insufficient number of input xps steps (<3)."<<endl;
+    return 0;
+  }
+  if(num_xas_steps <= 2){
+    cout<<"Error: insufficient number of input xas steps (<3)."<<endl;
+    return 0;
+  }
+  if(max_w<=min_w){
+    cout<<"Error: max_w <= min_w is not permissible."<<endl;
+    return 0;
+  }
+  
+  //Allocation
+  xps_freqs = new double[num_xps_steps];
+  xps = new double[num_xps_steps];
+  xas_freqs = new double[num_xas_steps];
+  xas = new double[num_xas_steps];
+  out_freqs = genGrid(num_w_steps,min_w,max_w); //Generate a uniform list of frequencies from scratch
+  out = new double[num_w_steps];  //This will be generated from the convolution
+}
 
 
 
