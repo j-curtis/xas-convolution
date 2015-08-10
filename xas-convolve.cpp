ac7c55d4d05d12cb8f4330d17b6dc9c79f1abc1e
j-curtis <jcurtis2@u.rochester.edu>
2015-08-09 21:40:25 -0700

    Update xas-convolve.cpp
    Added parsing and interpolating methods

--- xas-convolve.cpp
+++ xas-convolve.cpp
@@ -10,3 +10,67 @@
 #include <cmath>
 #include <complex>
 using namespace std;
+
+//These are methods for parsing the data file
+//They will evaluate label=="flag" and if it does, will assign variable to value (appropriately typed).
+//Each one parses for a different data type. 
+void parseStr(string label, string value, string flag, string & variable){
+  //This parses for a string type variable
+  if(label == flag){
+    variable = value;
+  }
+}
+void parseDbl(string label, string value, string flag, double & variable){
+  //This parses for a double type variable
+  if(label == flag){
+    variable = atof(value.c_str() );  //We convert the value to a double
+  }
+}
+void parseInt(string label, string value, string flag, int & variable){
+  //This parses for a double type variable
+  if(label == flag){
+    variable = atoi(value.c_str() );  //We convert the value to a double
+  }
+}
+
+//This function will linearly interpolate between the two values 
+//NOTE: this is not safe against division by zero resulting from x2 = x1
+double linearInterpolate(double x, double x1, double f1, double x2, double f2){
+  //Given x, x1, x2, f1, f2, this will interpolate f(x) as:
+  //f(x) = f1 if x<=x1
+  //f(x) = f1 + f2*(x-x1)/(x2-x1) if x1<x<=x2
+  //f(x) = f2 if x>x2
+  if(x<=x1){
+    return f1;
+  }
+  else if(x>x2){
+    return f2;
+  }
+  else{
+    return f1+f2*(x-x1)/(x2-x1);  //Note this is not safe against x2=x1==> division by zero error
+  }
+}
+
+//This function will snap a given point onto a grid 
+//It will return the largest array index that has a value less than the passed value
+//If the element is out of bounds, it will return 0 or arrysize-1 depending on which end of the array it crosses
+//It assumes that the array is uniformly spaced
+int snapToGrid(double x, int size, double * arry){
+  //First we deal with the case that x<values[0]
+  if(x<arry[0]){
+    return 0;
+  }
+  //Next, if it is in the array range
+  else if(x<arry[size-1]){
+    return floor( (x-arry[0])/(arry[1]-arry[0]) ); //We assume the array is uniform and has spacing values[1]-values[0]
+  }
+  else{
+    return size-1;  //It is larger than the largest entry so we return the largest index
+  }
+}
+
+
+
+
+
+
