6504c70946f6dcf32a87efc98f277f7e355c1872
j-curtis <jcurtis2@u.rochester.edu>
2015-08-09 21:06:13 -0700

    Started xas-convolve.cpp
    The main file for the convolution algorithm. It uses C++.

---
+++ xas-convolve.cpp
@@ -0,0 +1,12 @@
+//This is the main file for the convolution script
+//Jonathan Curtis
+//08/09/15
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <complex>
+using namespace std;
